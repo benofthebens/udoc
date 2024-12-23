5c7f7c390f8d8544c1e262cd5bcde1e154b9ad59
Benjamin <119005858+benofthebens@users.noreply.github.com>
2024-12-23 17:16:43 +0000

    test@cmd: added test for initialising the directory

--- src/cmd/commands.rs
+++ src/cmd/commands.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::fs;
+use std::io;
+use 
 use clap::Subcommand;
 
 #[derive(Subcommand, Debug)]
@@ -5,16 +9,60 @@ pub enum Commands {
     
     New {
         #[arg(short, long)]
-        path: String
+        path: String,
+        #[arg(short, long)]
+        name: String
+
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::fs;
+    use std::path::Path;
+
+    #[test]
+    fn init_test() -> io::Result<()> {
+
+        let test_path: String = "C:/Users/benja/Documents/udoc".to_string();
+        let test_name: String = "test".to_string();
+        let full_path = format!("{}/{}", test_path, test_name);
+        
+        let udoc_path = format!("{}/.udoc", &full_path);
+        let images_path = format!("{}/images", &full_path);
+        let videos_path = format!("{}/videos", &full_path);
+
+        Commands::new(&test_path, &test_name);
+        
+        assert!(Path::new(&full_path).exists());
+        assert!(Path::new(&udoc_path).exists());
+        assert!(Path::new(&images_path).exists());
+        assert!(Path::new(&videos_path).exists());
+
+        fs::remove_dir_all(&full_path)
+            .expect("Unable to delete directory");
+
+        Ok(())
+}
+
 impl Commands {
-    pub fn execute(&self) {
+    pub fn execute(&self) -> io::Result<()>{
         match self {
-            Commands::New { path } => Self::new(path)
+            Commands::New { path, name } => Self::new(path, name)
         }
     }
-    pub fn new(path: &String) {
-        println!("Create Directory here {}",*path);
+    pub fn new(path: &String, name: &String) -> io::Result<()> {
+
+        let full_path: String = format!("{path}/{name}");
+
+        fs::create_dir(&full_path);
+        fs::create_dir(format!("{}/.udoc", &full_path));
+        fs::create_dir(format!("{}/images", &full_path));
+        fs::create_dir(format!("{}/videos", &full_path));
+        
+        Ok(())
     }
 }
+
