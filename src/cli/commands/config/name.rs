41a31a648edb6f52bc137b8c17c1ddae09e74d5b
Benjamin <119005858+benofthebens@users.noreply.github.com>
2025-01-03 21:16:12 +0000

    chore@name: added root, data, config path variables

--- src/cli/commands/config/name.rs
+++ src/cli/commands/config/name.rs
@@ -4,14 +4,21 @@ use crate::config;
 use crate::config::Config;
 
 pub fn set_config_name(name: &String) -> io::Result<()> {
-	if !Path::new("./.udoc").exists() {
+
+	let binding = std::env::current_dir()?;
+	let root_path = binding.to_str().unwrap();
+
+	let data_path = format!("{root_path}/.udoc");
+	let config_path = format!("{data_path}/config.json");
+
+	if !Path::new(&data_path).exists() {
 		panic!("This path is not a udoc repository");
 	}
 
-	let mut config_json: Config = config::read_config("./.udoc/config.json".to_string());
-	config_json.user.username = name.to_string();
-	std::fs::remove_file("./.udoc/config.json")?;
-	config::create_config(&"./.udoc".to_string(), config_json).expect("TODO: panic message");
+	let mut config: Config = config::read_config(&config_path);
+	config.user.username = name.to_string();
+	std::fs::remove_file(&config_path)?;
+	config::create_config(&data_path, config).expect("TODO: panic message");
 
 	Ok(())
 }
