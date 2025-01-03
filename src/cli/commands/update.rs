dbfc9e15e2670c98bbf123254a8004647d630008
Benjamin <119005858+benofthebens@users.noreply.github.com>
2025-01-03 20:36:12 +0000

    chore@update: removed redundant variables and standardised path names

--- src/cli/commands/update.rs
+++ src/cli/commands/update.rs
@@ -4,28 +4,27 @@ use std::path::Path;
 use crate::{config, log};
 
 pub fn update() -> io::Result<()> {
+
 	let binding = std::env::current_dir()?;
-	let path: Option<&str> = binding.to_str();
-	let path: &str = match path {
-		Some(path) => path,
-		None => panic!("Path provided is None"),
-	};
-	let config_path = format!("{path}/.udoc");
-	let config_json = config::read_config(format!("{config_path}/config.json"));
-	if !Path::new(&config_path).exists() {
+	let root_path: &str = binding.to_str().unwrap();
+	let data_path = format!("{root_path}/.udoc");
+	let config_path = format!("{data_path}/config.json");
+	let log_path = format!("{root_path}/log.md");
+
+	let repo_config = config::read_config(&config_path);
+
+	if !Path::new(&data_path).exists() {
 		panic!("This path is not a udoc repository");
-	} else if config_json.user.email == String::new() || config_json.user.username == String::new() {
+	} else if repo_config.user.email == String::new() || repo_config.user.username == String::new() {
 		panic!("Please choose give your name and email");
 	}
 
 	let mut file = OpenOptions::new()
 		.append(true)
 		.create(true)
-		.open(format!("{path}/log.md"))?;
+		.open(log_path)?;
 
-	let root_path = path.to_string();
-
-	log::update_images(&mut file, &root_path)
+	log::update_images(&mut file, &root_path.to_string())
 		.expect("TODO: panic message");
 
 	Ok(())
