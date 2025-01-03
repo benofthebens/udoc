5210651e6ff19e8384180b3e3d36ced1e70821e1
Benjamin <119005858+benofthebens@users.noreply.github.com>
2025-01-03 21:18:09 +0000

    chore@reset:  added root, data, config path variables

--- src/cli/commands/reset.rs
+++ src/cli/commands/reset.rs
@@ -4,14 +4,21 @@ use crate::config;
 use crate::config::{Config, User};
 
 pub fn reset() -> io::Result<()> {
-	if !Path::new("./.udoc").exists() {
+	let binding = std::env::current_dir()?;
+	let root_path = binding.to_str().unwrap();
+
+	let data_path = format!("{root_path}/.udoc");
+	let config_path = format!("{data_path}/config.json");
+
+	if !Path::new(&data_path).exists() {
 		panic!("This is not a udoc repository ");
 	}
-	fs::remove_dir_all("./.udoc").expect("TODO: panic message");
-	fs::create_dir("./.udoc");
+
+	fs::remove_dir_all(&data_path).expect("TODO: panic message");
+	fs::create_dir(&data_path).expect("Unable to create directory");
 
 	config::create_config(
-		&"./.udoc".to_string(),
+		&config_path,
 		Config::new(
 			1,
 			"log.md".to_string(),
