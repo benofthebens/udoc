2a0e80ecd15e9b6262c2e6671d82ac6c535ea52b
Benjamin <119005858+benofthebens@users.noreply.github.com>
2025-01-03 21:13:51 +0000

    chore@email: added root_path, data_path, config_path variables

--- src/cli/commands/config/email.rs
+++ src/cli/commands/config/email.rs
@@ -4,13 +4,21 @@ use crate::config;
 use crate::config::Config;
 
 pub fn set_config_email(email: &String) -> io::Result<()> {
-	if !Path::new("./.udoc").exists() {
+	let binding = std::env::current_dir()?;
+	let root_path = binding.to_str().unwrap();
+
+	let data_path = format!("{root_path}/.udoc");
+	let config_path = format!("{data_path}/config.json");
+
+	if !Path::new(&data_path).exists() {
 		panic!("This path is not a udoc repository");
 	}
 
-	let mut config_json: Config = config::read_config("./.udoc/config.json".to_string());
+	let mut config_json: Config = config::read_config(&config_path);
+
 	config_json.user.email = email.to_string();
-	config::create_config(&"./.udoc".to_string(), config_json).expect("TODO: panic message");
+
+	config::create_config(&config_path, config_json).expect("TODO: panic message");
 
 	Ok(())
 }
