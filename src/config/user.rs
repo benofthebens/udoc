37510776aa592652c6e9933eba79ea26be790e5e
Benjamin <119005858+benofthebens@users.noreply.github.com>
2025-01-04 22:23:46 +0000

    feature@user: added Clone

--- src/config/user.rs
+++ src/config/user.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct User {
     pub username: String,
     pub email: String,
