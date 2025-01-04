2f9cbf49c57315619b82d5330c08729691eea217
Benjamin <119005858+benofthebens@users.noreply.github.com>
2025-01-04 22:32:32 +0000

    feature@utils: added Paths enum to return the paths

---
+++ src/cli/utils/paths.rs
@@ -0,0 +1,24 @@
+use crate::config::Config;
+
+pub enum Paths {
+    Root,
+    Data,
+    Config,
+    Log,
+}
+impl Paths {
+    pub fn get(&self) -> String {
+        let root_path = std::env::current_dir()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        match self {
+            Paths::Root => root_path,
+            Paths::Data => format!("{root_path}/.udoc"),
+            Paths::Config => format!("{root_path}/.udoc/config.json"),
+            Paths::Log => format!("{root_path}/log.md"),
+        }
+    }
+}
