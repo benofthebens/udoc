bd3e9393e8869a6d9c3e8630cb28ce40f57204c7
Benjamin Whalley <benjaminjwhalley@outlook.com>
2025-03-21 16:49:44 +0000

    feature@exchange: added read_exchange file function added rename to the Exchange struct to ensure it can be deserialised

--- src/log/exchange.rs
+++ src/log/exchange.rs
@@ -1,11 +1,13 @@
 mod html;
 
 use std::{fs, io};
+use quick_xml::de::from_str;
 use quick_xml::se::Serializer;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use crate::log::Section;
 
-#[derive(Debug, Default, Serialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename = "error")]
 pub struct Exchange {
     pub title: String,
     pub section: Vec<Section>,
@@ -34,14 +36,25 @@ impl ExchangeItem {
            }
     }
 }
-pub fn create_exchange_file(exchange_path: &String, contents: Exchange) -> Result<String, io::Error> {
-    let mut buffer = String::new();
+pub fn create_exchange_file(exchange_path: &String, contents: &Exchange) -> Result<String, io::Error> {
+    let mut buffer = String::new(); // create a buffer to write to
     fs::create_dir_all(exchange_path)?;
     let mut ser = Serializer::with_root(&mut buffer, Some("error"))
         .unwrap();
+    // Serialise the Exchange object to xml
     ser.indent(' ', 4);
     contents.serialize(ser).unwrap();
     fs::write(format!("{exchange_path}/exchange.xml"), &buffer)?;
-
     Ok(buffer)
+}
+pub fn read_exchange_file(exchange_path: &String) -> Result<Exchange, io::Error> {
+    // Read the contents of the file into a string
+    let file_path = format!("{}/exchange.xml", exchange_path);
+    let file_content = fs::read_to_string(file_path)?;
+    println!("{file_content}");
+
+    // Deserialize the XML string into an Exchange struct
+    let exchange: Exchange = from_str(&file_content).unwrap();
+
+    Ok(exchange)
 }
